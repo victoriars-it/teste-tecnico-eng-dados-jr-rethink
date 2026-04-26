64099f62c0bd7fb2612e2e2df6bbba948cc620ef
Victoria Resende <victoriars.it@gmail.com>
2026-04-26 19:08:24 -0300

    configura SparkSession para Delta Lake e adiciona delta-spark nas dependencias

--- utils.py
+++ utils.py
@@ -10,6 +10,8 @@ def get_spark_session(app_name: str) -> SparkSession:
     spark = SparkSession.builder \
         .appName(app_name) \
         .master("local[*]") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .getOrCreate()
     
     spark.sparkContext.setLogLevel("WARN")
