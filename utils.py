160cda0889920a68d5d7a84085237f1133e8fcbf
Victoria Resende <victoriars.it@gmail.com>
2026-04-26 19:21:13 -0300

    adiciona spark.jars.packages no utils.py pra carregar JARs do Delta no classpath

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ def get_spark_session(app_name: str) -> SparkSession:
     spark = SparkSession.builder \
         .appName(app_name) \
         .master("local[*]") \
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .getOrCreate()
